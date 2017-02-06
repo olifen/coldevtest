1f4c5989af35cc5ffd80e81d2afb2e00b30568fd
Oliver <of9142@my.bristol.ac.uk>
2017-02-06 16:51:21 +0000

    Colossus Bets dev test first commit - incomplete

---
+++ js/app.js
@@ -0,0 +1,66 @@
+angular.module('ColDevTest', [])
+  .controller('PoolsController', PoolsController)
+  .controller('BetsController', BetsController)
+
+  .controller('BetController', ['$scope', function($scope) {
+    $scope.bet = [];
+    $scope.submit = function() {
+      if ($scope.text) {
+        $scope.bet.push(this.value);
+        $scope.value = '';
+      }
+      console.log('you clicked submit');
+      console.log($scope.bet.stake);
+      console.log($scope.bet.leg_1);
+    };
+  }]);
+
+
+function BetsController() {
+  this.bets =
+    { stake: '', leg_1: '', leg_2: '', leg_3: '' }
+;
+  console.log(this.bets.stake);
+
+  this.add = addBet;
+  this.newBet = '' ;
+  function addBet() {
+    this.bets.push('');
+    this.newBet.bet = '';
+  }
+
+}
+
+function PoolsController() {
+  this.all = [
+    { name: 'Colossus',
+      prize: '£10,000,000',
+      start_time: '17:00',
+      leg_1: 'Liverpool v Tottenham',
+      leg_2: 'Wolves v Newcastle',
+      leg_3: 'Fiorentina v Udinese',
+      leg_4: 'Villarreal v Malaga',
+      leg_5: 'Sassuolo v Chievo',
+      leg_6: 'Las Palmas v Sevilla',
+      leg_7: 'Eibar v Granada'
+   }
+  ];
+}
+
+
+
+// //get pools data from the API
+// BetsController.$inject = ['$http'];
+// function BetsController($http) {
+//   var self = this;
+//   self.all = [];
+//
+//   function getPools() {
+//     $http
+//       .get('https://colossusdevtest.herokuapp.com/api/pools.json')
+//       .then(function(response) {
+//         self.all = response.data.pools;
+//       });
+//   }
+//   getPools();
+// }
